bfda17b46ab78f973ad0ac5cc4e0b6dec0b04e3a
qfastok <denis.yakovlv@gmail.com>
2021-05-31 16:48:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ import random
 for i in (3, 5, 9):
     print(sum(range(1, 101, i)))
 
+# 1 line
+# for n in (3, 5, 9): print(sum(range(1,101,n)))
+
 # Є словник | ключ- без різниці | value - 20 елементів інтові
 # | вивести ключі трьох найбільший значень в порядку зростання значень
 my_dict = dict((key, random.randint(0, 100)) for key in range(20))
